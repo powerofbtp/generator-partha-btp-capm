dca86221066070d706ea9a772a00400bdeabc278
powerofbtp <powerofbtp@gmail.com>
2022-06-10 21:42:31 +0200

    2nd commit

--- generators/app/templates/srv/catalog-service.js
+++ generators/app/templates/srv/catalog-service.js
@@ -2,24 +2,24 @@ const cds = require('@sap/cds');
 
 module.exports = cds.service.impl(async function() {
 
-    this.on('READ', "Computers", async (req) => {
+    // this.on('READ', "Computers", async (req) => {
 
-        const computers = await cds.tx(req).run(req.query);
-        const computers_final = [];
+    //     const computers = await cds.tx(req).run(req.query);
+    //     const computers_final = [];
 
-        console.log(computers);
+    //     console.log(computers);
         
-        for (i=0; i<computers.length; i++)
-        {
+    //     for (i=0; i<computers.length; i++)
+    //     {
             
-            if (computers[i].name === 'HP')
-            {
-                computers[i].name = Hewlett-Packard;
-                computers_final.push(computers[i])
-            }
-        }
+    //         if (computers[i].name === 'HP')
+    //         {
+    //             computers[i].name = Hewlett-Packard;
+    //             computers_final.push(computers[i])
+    //         }
+    //     }
         
-        return computers_final;
+    //     return computers_final;
 
-    })
+    // })
 })
